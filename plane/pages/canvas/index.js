f5427b0c69e8d2b5dd24283a3f79c59003d7d5a6
zhaoshuxue <zhaoshuxue@163.com>
2018-02-01 00:19:24 +0800

    commitBy2018-02-01-00:19:24

--- plane/pages/canvas/index.js
+++ plane/pages/canvas/index.js
@@ -1,40 +1,36 @@
 // pages/canvas/index.js
+
+var context;
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    state: 1,
     width: 100,
     height: 100,
     y1: 0,
     y2: 0,
-    time: 1
+    time: 1,
+
+    //
+    planeWidth: 99,
+    planeHeight: 124,
+    x: 0,
+    y: 0,
+
+    //
+    bulletWidth: 9,
+    bulletHeight: 21,
+
+    bullets: []
   },
 
   canvasIdErrorCallback: function (e) {
     console.error(e.detail.errMsg)
-  },
-
-  zhao: function () {
-    console.log(1)
-    var context = wx.createCanvasContext('firstCanvas')
-    console.log(context)
-
-    // 480  852
-    // context.drawImage('../../img/background.png', 0, 0, 375, 603)
-
-    context.drawImage('../../img/background.png', 0, 0, this.data.width, this.data.height)
-    context.draw()
-
-
-    // wx.chooseImage({
-    //   success: function (res) {
-    //     context.drawImage(res.tempFilePaths[0], 0, 0, 150, 100)
-    //     context.draw()
-    //   }
-    // })
-
   },
 
   /**
@@ -42,15 +38,17 @@ Page({
    */
   onLoad: function (options) {
     var that = this;
+    console.log(10)
     wx.getSystemInfo({
       success: function (res) {
-        console.info(res)
         that.setData({
-          width: res.windowWidth - 2,
-          height: res.windowHeight - 2
+          width: res.windowWidth,
+          height: res.windowHeight
         })
       },
     })
+
+    context = wx.createCanvasContext('firstCanvas')
   },
 
   /**
@@ -58,7 +56,7 @@ Page({
    */
   onReady: function () {
     // 使用 wx.createContext 获取绘图上下文 context
-    var context = wx.createCanvasContext('firstCanvas')
+    // var context = wx.createCanvasContext('firstCanvas')
 
     context.setStrokeStyle("#00ff00")
     context.setLineWidth(5)
@@ -82,42 +80,44 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    this.data.y1 = -this.data.height;
+    // console.log(1)
+    this.init();
     this.gameGo();
+  },
+
+  init: function () {
+    this.data.y1 = -this.data.height;
+    this.data.y2 = 0;
+    // 
+    this.data.x = (this.data.width - this.data.planeWidth) / 2;
+    this.data.y = this.data.height - this.data.planeHeight - 10;
 
   },
 
   gameGo: function () {
     const that = this;
     // console.table([["John", "Smith"]])
-    console.log(new Date())
-    var context = wx.createCanvasContext('firstCanvas')
+    // console.log(new Date())
 
-    console.log(this.data)
 
-    context.drawImage('../../img/background.png', 0, this.data.y1, this.data.width, this.data.height);//第一张图片
-    context.drawImage('../../img/background.png', 0, this.data.y2, this.data.width, this.data.height);//第二张图片
+    var sky = this.BgSky();
+    sky.paint()
+    sky.step()
+
+    //
+    var hero = this.Hero();
+    hero.paint();
+    hero.step();
+
+    var bullet = this.Bullet()
+    bullet.paint();
+    bullet.step();
 
 
     context.draw()
-
-    this.data.time++;
-    if (this.data.time % 3 == 0) {//控制背景图片height值的增加
-      this.data.y1++;//图片运动下一帧
-      this.data.y2++;
-      //图片移动处画布后将y坐标重置为-height 实现图片衔接滚动
-      this.data.y1 > this.data.height && (this.data.y1 = -this.data.height);
-      this.data.y2 > this.data.height && (this.data.y2 = -this.data.height);
-      this.data.time = 1;//重置移动时间
-    }
-    
-    var sky = this.BgSky(1);
-    sky.paint()
-    console.log(sky)
-
     setTimeout(function () {
       that.gameGo();
-    }, 10000);
+    }, 10);
   },
 
   /**
@@ -155,14 +155,120 @@ Page({
 
   },
 
-  BgSky: function(num){
-    
+  // 背景图片
+  BgSky: function () {
+    let that = this;
+    // var context = wx.createCanvasContext('firstCanvas')
 
-    var a = function paint(){
-      console.log('方法体内' + num)
+    var paint = function paint() {
+      context.drawImage('../../img/background.png', 0, that.data.y1, that.data.width, that.data.height);//第一张图片
+      context.drawImage('../../img/background.png', 0, that.data.y2, that.data.width, that.data.height);//第二张图片
+      // context.draw()
+    }
+
+    var step = function step() {
+      that.data.time++;
+      if (that.data.time % 3 == 0) {//控制背景图片height值的增加
+        that.data.y1++;//图片运动下一帧
+        that.data.y2++;
+        //图片移动处画布后将y坐标重置为-height 实现图片衔接滚动
+        that.data.y1 > that.data.height && (that.data.y1 = -that.data.height);
+        that.data.y2 > that.data.height && (that.data.y2 = -that.data.height);
+        that.data.time = 1;//重置移动时间
+      }
     }
     return {
-      paint : a
+      paint: paint,
+      step: step
     }
+  },
+
+  Hero: function () {
+    let that = this;
+    // var context = wx.createCanvasContext('firstCanvas')
+
+    var paint = function paint() {
+      context.drawImage('../../img/hero1.png', that.data.x, that.data.y, 99, 124);
+      // context.draw()
+    }
+
+    var step = function step() {
+
+    }
+
+    return {
+      paint: paint,
+      step: step
+    }
+  },
+
+  Bullet: function () { 
+    let that = this;
+
+    var paint = function paint() {
+      context.drawImage('../../img/bullet1.png', that.data.x, that.data.y, 9, 21);
+    }
+
+    var step = function step() {
+
+    }
+
+    return {
+      paint: paint,
+      step: step
+    }
+  },
+  fun2: function () { },
+  fun3: function () { },
+  touchStart: function (e) {
+    // console.log(e.touches[0].x)
+    // console.log(e.touches[0].y)
+    // console.log(this.data.x)
+    // console.log(this.data.y)
+    var x = e.touches[0].x
+    var y = e.touches[0].y
+
+    var w = this.data.planeWidth
+    var h = this.data.planeHeight
+
+    if (x > this.data.x && x < this.data.x + w) {
+      if (y > this.data.y && y < this.data.y + h) {
+        this.data.state = 2
+      }
+    }
+
+    // console.log(this.data.state)
+    // console.log(this.data.x)
+    // console.log(this.data.y)
+  },
+  touchMove: function (e) {
+    var that = this;
+    // console.log(this.data.state)
+    if (this.data.state != 2){
+      return;
+    }
+
+    // console.log(e.touches[0].x)
+    // console.log(e.touches[0].y)
+
+    var left = e.touches[0].x;
+    var top = e.touches[0].y;
+
+    var w = this.data.planeWidth
+    var h = this.data.planeHeight
+
+    left = left - (w/2)
+    top = top - (h/2)
+
+    this.data.x = left;
+    this.data.y = top;
+  },
+  touchEnd: function (e) {
+    console.log(11111111)
+    this.data.state = 1
   }
+
+
+
+
 })
