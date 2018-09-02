1cc3c01721e39841089e0bd9747d9a055192b9e3
zhaoshuxue <zhaoshuxue@163.com>
2018-09-03 01:13:23 +0800

    commitBy2018-09-03-01:13:22

--- plane/pages/puzzle/puzzle.js
+++ plane/pages/puzzle/puzzle.js
@@ -2,20 +2,25 @@ const Puzzle = require("./h5puzzle.js");
 Page({
   data: {
     imgPoints: [],
-    imgArr: ['	https://highness-1-1253922088.cos.ap-beijing.myqcloud.com/zhao/puzzle.png ','../../src/images/quanyecha.jpg', '../../src/images/haidao.jpg', '../../src/images/mingren1.jpg', '../../src/images/qilongzhu1.jpg', '../../src/images/quanye.jpg'],
+    imgArr: [
+      'https://highness-1-1253922088.cos.ap-beijing.myqcloud.com/zhao/puzzle.png',
+      'https://highness-1-1253922088.cos.ap-beijing.myqcloud.com/zhao/puzzle.png',
+      'https://highness-1-1253922088.cos.ap-beijing.myqcloud.com/zhao/puzzle.png',
+      'https://highness-1-1253922088.cos.ap-beijing.myqcloud.com/zhao/puzzle.png'
+    ],
     imgUrl: '../../src/images/quanyecha.jpg',
     levelArr: [{
         id: 2,
-        text: '弱智'
+        text: '2x2'
       },
       {
         id: 3,
-        text: '简单'
+        text: '3x3'
       },
       {
         id: 4,
-        text: '普通'
-      },
+        text: '4x4'
+      }/*,
       {
         id: 5,
         text: '困难'
@@ -43,7 +48,7 @@ Page({
       {
         id: 11,
         text: '已死'
-      }
+      }*/
     ],
     WIDTH: 0,
     HEIGHT: 0,
@@ -58,23 +63,41 @@ Page({
     type0: 4
   },
   onReady() {
-    let _this = this;
-
+    // let _this = this;
     new Puzzle(this);
   },
   getType(e) {
     this.setData({
-      trans: -this.data.WIDTH,
+      // trans: -this.data.WIDTH,
       type0: e.currentTarget.dataset.type
     })
     this.puzzle = new Puzzle(this, {
       type: e.currentTarget.dataset.type
+    })
+
+    // todo
+    this.setData({
+      trans: -this.data.WIDTH * 2,
+      imgUrl: 'https://highness-1-1253922088.cos.ap-beijing.myqcloud.com/zhao/puzzle.png'
     })
   },
   getUrl(e) {
     this.setData({
       trans: -this.data.WIDTH * 2,
       imgUrl: e.currentTarget.dataset.url
+    })
+  },
+
+// ************************************************
+  selectImage(e){
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        // var tempFilePaths = res.tempFilePaths
+      }
     })
   }
 })
