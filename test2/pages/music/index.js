9a69c3e1af8abe8a63d33cb4094a495d86cd4eea
zhaoshuxue <zhaoshuxue@163.com>
2019-03-01 18:03:40 +0800

    commitBy2019-03-01-18:03:39

--- test2/pages/music/index.js
+++ test2/pages/music/index.js
@@ -21,7 +21,10 @@ Page({
   onLoad: function (options) {
     let that = this;
     innerAudioContext.autoplay = false
-    innerAudioContext.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
+    // innerAudioContext.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
+
+    innerAudioContext.src = 'http://tmp/wxbc6e55a1c09739ad.o6zAJs3t2F5eLi0PQpqv00f1QjTs.kNMHkwmDGf89e024c03d0df5c3e9a9d3429f32ebe414.durationTime=6459.mp3';
+
     innerAudioContext.onPlay(() => {
       console.log('开始播放')
       console.log(innerAudioContext)
