0879d83cd8de1ff377d914da95c0f6ef8b38bb4d
zhaoshuxue <zhaoshuxue@163.com>
2019-03-11 23:05:58 +0800

    commitBy2019-03-11-23:05:57

--- Images/miniprogram/pages/photo/index.js
+++ Images/miniprogram/pages/photo/index.js
@@ -140,5 +140,13 @@ Page({
           })
         }
       })
+  },
+
+  showPhoto: function(e){
+    var url = e.currentTarget.dataset.url;
+    wx.previewImage({
+      // current: '', // 当前显示图片的http链接
+      urls: [url] // 需要预览的图片http链接列表
+    })
   }
 })
