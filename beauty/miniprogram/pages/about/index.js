9611e939034dda414cc1966b68f776e3c1ef7e97
zhaoshuxue <zhaoshuxue@163.com>
2020-09-15 01:19:37 +0800

    commitBy2020-09-15-01:19:36

--- beauty/miniprogram/pages/about/index.js
+++ beauty/miniprogram/pages/about/index.js
@@ -1,10 +1,15 @@
 // miniprogram/pages/about/index.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    theme: app.globalData.theme,
+
+    avatarUrl: '/images/user-unlogin.png',
+    userInfo: {},
 
   },
 
@@ -12,7 +17,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          console.info("获取用户信息")
+          wx.getUserInfo({
+            success: res => {
+              console.info("获取用户信息", res)
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
   },
 
   /**
